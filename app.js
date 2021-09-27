f53c72d564c5e2dd2cbd88c8892ee5537b4ef514
ohohopen <ohohopen@gmail.com>
2021-09-27 08:16:30 +0800

    基礎頁面設置＆app啟動

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const port = 3000;
+const app = express();
+app.engine("hbs", exphbs({ defaultLayout: "main", extname: "hbs" }));
+app.set("view engine", "hbs");
+app.get("/", (req, res) => res.render("index"));
+app.listen(port, () => {
+	console.log("app.js is listening");
+});
