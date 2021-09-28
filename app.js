b951bbffe569c810e4f743e2293ff593366973fd
ohohopen <ohohopen@gmail.com>
2021-09-28 09:02:46 +0800

    前端：新增單筆編輯頁 後端：新增＂編輯單筆資料＂功能

--- app.js
+++ app.js
@@ -64,6 +64,43 @@ app.get("/todos/:id", (req, res) => {
 		.then((todos) => res.render("show", { todos }))
 		.catch((error) => console.log(error));
 });
+//前往修改單筆資料之頁面
+app.get("/todos/:id/edit", (req, res) => {
+	const id = req.params.id;
+	Todo.findById(id)
+		.lean()
+		.then((todos) => res.render("edit", { todos }))
+		.catch((error) => console.log(error));
+});
+//修改單筆資料
+app.post("/todos/:id/edit", (req, res) => {
+	const id = req.params.id;
+	const name = req.body.name,
+		name_en = req.body.name_en,
+		category = req.body.category,
+		image = req.body.image,
+		location = req.body.location,
+		phone = req.body.phone,
+		google_map = req.body.google_map,
+		rating = req.body.rating,
+		description = req.body.description;
+
+	return Todo.findById(id)
+		.then((todos) => {
+			todos.name = name;
+			todos.name_en = name_en;
+			todos.category = category;
+			todos.image = image;
+			todos.location = location;
+			todos.phone = phone;
+			todos.google_map = google_map;
+			todos.rating = rating;
+			todos.description = description;
+			todos.save();
+		})
+		.then(() => res.redirect("/"))
+		.catch((error) => console.log(error));
+});
 
 //監聽app.js
 app.listen(port, () => {
